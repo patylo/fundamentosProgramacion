795b5a7b7b7fbd1f1e18574c97dcbf5d7658f249
Paty <Paty@192.168.1.81>
2019-09-28 13:33:18 -0500

    se crea objeto calculadora

--- src/main/java/org/edu/ito/Ejemplo/App.java
+++ src/main/java/org/edu/ito/Ejemplo/App.java
@@ -13,5 +13,13 @@ public class App
         System.out.println( "Hola mundo" );
         //String nombre = JOptionPane.showInputDialog("Escribe tu nombre");
         //JOptionPane.showMessageDialog(null, "Bienvenido: "+nombre);
+        int resultado=0; 
+        int a=10;
+        int b=20;
+        resultado=a + b;
+        System.out.println( resultado );
+        
+        Calculadora calculadora=new Calculadora();
+        System.out.println( calculadora.suma(100, 50) );
     }
 }
